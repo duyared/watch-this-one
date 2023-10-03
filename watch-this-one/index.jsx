a5475699589431cc981ea08fe23048a1e0705157
yared <yaredte45@gmail.com>
2023-10-03 18:56:06 +0300

    add new entry point - index.jsx

---
+++ watch-this-one/index.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+function App(){
+    return (
+        <h1>hello there</h1>
+    )
+}
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
